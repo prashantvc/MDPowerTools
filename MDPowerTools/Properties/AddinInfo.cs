887752832ecba9999842d00fd01b1d9f7a117ea0
Prashant Cholachagudda <pvc@outlook.com>
2016-02-08 19:59:03 +0530

    Initial commit

---
+++ MDPowerTools/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin (
+	"MDPowerTools",
+	Namespace = "MDPowerTools",
+	Version = "1.0"
+)]
+
+[assembly: AddinName ("MDPowerTools")]
+[assembly: AddinCategory ("IDE extensions")]
+[assembly: AddinDescription ("MDPowerTools")]
+[assembly: AddinAuthor ("Prashant Cholachagudda")]
+
